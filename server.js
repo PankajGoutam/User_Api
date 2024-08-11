902da77a29a6ef9da71916d993b496544462ba14
Aayan Goutam <121614824+PankajGoutam@users.noreply.github.com>
2024-08-11 20:24:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:4173'  
+  origin: 'http://localhost:5173'  
 }));
 
 app.use('/api/v1/users', userRoutes);
